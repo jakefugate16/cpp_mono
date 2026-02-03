c4e40fccd727f64c0b774987a0cf8d64ce533dcf
jakefugate16 <jakefugate16@gmail.com>
2026-02-03 15:20:39 -0500

    qt examples

--- qt/dialog/include/window.h
+++ qt/dialog/include/window.h
@@ -8,43 +8,35 @@
 #include <QtWidgets/QRadioButton>
 #include <QtWidgets/QDialogButtonBox>
 
-class BaseWindow
+namespace ui{
+	class MainWindow;
+	class Window2;
+}
+
+
+class ui::MainWindow : public QWidget
 {
+
 public:
-	explicit BaseWindow(QWidget* parent = nullptr);
+	explicit MainWindow(QWidget* parent = nullptr);
+
 private:
-	QDialogButtonBox* buttonbox;
+	QGridLayout* grid;
+	
+	QGroupBox* group;
+	QVBoxLayout* vbox;
+	QRadioButton* m_button1;
+	QRadioButton* m_button2;
+	QRadioButton* m_button3;
+
 	QPushButton* m_button_ok;
 	QPushButton* m_button_cancel;
 };
 
-class Window1 : public BaseWindow
-{
-
-public:
-	explicit Window1(QWidget* parent = nullptr) : BaseWindow(parent);
-
-private:
-	QPushButton* m_button1;
-	QPushButton* m_button2;
-	QPushButton* m_button3;
-	QVBoxLayout* vbox1;
-	
-	QRadioButton* m_rbutton1;
-	QRadioButton* m_rbutton2;
-	QRadioButton* m_rbutton3;
-	QVBoxLayout* vbox2;
-
-	QGroupBox* groupbox1;
-	QGroupBox* groupbox2;
-	QHBoxLayout* hbox;
-
-};
-
-class Window2 : public BaseWindow
+class ui::Window2 : public QWidget
 {
 public:
-	explicit Window2(QWidget* parent = nullptr) : BaseWindow(parent);
+	explicit Window2(QWidget* parent = nullptr);
 private:
 	QPushButton* m_button1;
 	QPushButton* m_button2;
--- qt/webcam/main.cpp
+++ qt/webcam/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
 	
-	ui::MainWindow window;
+	MainWindow window;
 	window.show();
 
 	return app.exec();
